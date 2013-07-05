8e7d9a5ed0f3cd4d9bd93d8e41749947ab3a352c
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2013-07-05 11:41:25 +0200

    Backported GEOT-4477 and GEOT-4484, support for localized strings in Title and Abstract SLD elements

--- modules/library/main/src/test/java/org/geotools/styling/SLDStyleTest.java
+++ modules/library/main/src/test/java/org/geotools/styling/SLDStyleTest.java
@@ -23,6 +23,7 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.Set;
 
 import junit.framework.Test;
@@ -138,6 +139,27 @@ public class SLDStyleTest extends TestCase {
         assertNotNull(xml);
         //we're content for the moment if this didn't throw an exception...
         //TODO: convert the buffer/resource to a string and compare
+    }
+    
+    /**
+     * XML --> SLD --> XML 
+     * @throws Exception
+     */
+    public void testSLDParserWithLocalizedTitle() throws Exception {
+        java.net.URL surl = TestData.getResource(this, "example-localized-sld.xml");
+        SLDParser stylereader = new SLDParser(sf, surl);
+        StyledLayerDescriptor sld = stylereader.parseSLD();
+        
+        //convert back to xml again
+        SLDTransformer aTransformer = new SLDTransformer();
+        String xml = aTransformer.transform(sld);
+
+        assertNotNull(xml);
+        assertTrue(xml.contains("<sld:Title>title"));
+        assertTrue(xml.contains("<sld:Localized lang=\""+Locale.ITALIAN.toString()+"\">titolo</sld:Localized>"));
+        assertTrue(xml.contains("<sld:Localized lang=\""+Locale.FRENCH.toString()+"\">titre</sld:Localized>"));
+        assertTrue(xml.contains("<sld:Localized lang=\""+Locale.CANADA_FRENCH.toString()+"\">titre</sld:Localized>"));
+        
     }
     
     public void testEmptyElements() throws Exception {
